f9d72fb9525a9394a01ca91be0c59672fb927fd7
up899590 <60944028+up899590@users.noreply.github.com>
2020-03-13 00:05:46 +0000

    Colour of the day
    Added colour of the day feature which will be different depending on the day

--- ColorPicker/app/src/main/java/com/example/colorpicker/MainActivity.java
+++ ColorPicker/app/src/main/java/com/example/colorpicker/MainActivity.java
@@ -7,8 +7,15 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import android.widget.TextView;
+import android.graphics.*;
+import android.widget.ImageView;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView cotd_description;
+    private ImageView cotd_image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,48 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+        loadColourOfTheDay();
     }
+
+    public void loadColourOfTheDay() {
+        cotd_description = (TextView) findViewById(R.id.text_colour_description);
+        cotd_image = (ImageView) findViewById(R.id.image_cotd);
+
+        Calendar c = Calendar.getInstance();
+        int day = c.get(Calendar.DAY_OF_WEEK);
+
+        switch (day) {
+            case Calendar.SUNDAY:
+                cotd_description.setText("Today's colour of the day is Orange. This colour is known to represent an adventurous nature and joy or perhaps a time of change.");
+                cotd_image.setColorFilter(Color.rgb(240, 150, 35));
+                break;
+            case Calendar.MONDAY:
+                cotd_description.setText("Today's colour of the day is White. This colour is known to represent purity, innocence and cleanliness.");
+                cotd_image.setColorFilter(Color.rgb(240, 230, 220));
+                break;
+            case Calendar.TUESDAY: 
+                cotd_description.setText("Today's colour of the day is Red. This colour is known to represent energy, strength, power and desire.");
+                cotd_image.setColorFilter(Color.rgb(200, 0, 0));
+                break;
+            case Calendar.WEDNESDAY: 
+                cotd_description.setText("Today's colour of the day is Green. This colour is known to represent wealth, renewal and to be the colour of nature.");
+                cotd_image.setColorFilter(Color.rgb(90, 230, 50));
+                break;
+            case Calendar.THURSDAY:
+                cotd_description.setText("Today's colour is Light Yellow. This colour is known to represent insight, enlightenment and happiness.");
+                cotd_image.setColorFilter(Color.rgb(230, 240, 60));
+                break;
+            case Calendar.FRIDAY:
+                cotd_description.setText("Today's colour is Blue. This colour is known to represent tranquility, calmness and stability, often associated with the sky and sea.");
+                cotd_image.setColorFilter(Color.rgb(30, 20, 190));
+                break;
+            case Calendar.SATURDAY:
+                cotd_description.setText("Today's colour is Violet. This colour is known to represent creativity, inspiration and ambition.");
+                cotd_image.setColorFilter(Color.rgb(240,130,240));
+                break;
+        }
+    }
+
 
 }
