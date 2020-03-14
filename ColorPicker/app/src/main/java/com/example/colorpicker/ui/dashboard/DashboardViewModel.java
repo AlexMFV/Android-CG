111fd922233cb4bcb72e5f1be83b3b69fdc423a4
AlexMFV <alexmfv1@gmail.com>
2020-03-14 17:48:42 +0000

    Fixed broken layout
    - Moved activity main to respective XML file.
    - Code cleanup.
    - Home Page logic made by up899590 at [#f9d72fb](https://github.com/AlexMFV/Android-CG/commit/f9d72fb9525a9394a01ca91be0c59672fb927fd7)
    
    Co-Authored-By: up899590 <up899590@users.noreply.github.com>

--- ColorPicker/app/src/main/java/com/example/colorpicker/ui/dashboard/DashboardViewModel.java
+++ ColorPicker/app/src/main/java/com/example/colorpicker/ui/dashboard/DashboardViewModel.java
@@ -1,23 +1,24 @@
 package com.example.colorpicker.ui.dashboard;
 
+import android.graphics.Color;
 import android.media.Image;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ScrollView;
+import android.widget.TextView;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.colorpicker.R;
+
+import java.util.Calendar;
+
 public class DashboardViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
-
     public DashboardViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is where the color picker goes!");
-    }
 
-    public LiveData<String> getText() {
-        return mText;
     }
 }
