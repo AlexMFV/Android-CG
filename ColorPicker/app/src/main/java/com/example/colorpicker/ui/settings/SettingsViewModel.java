93f0ea60f680ef6dd9371b813fad88165622e7dd
AdamGui <AdamGui@users.noreply.github.com>
2020-03-14 21:04:55 +0000

    Added settings nav bar

---
+++ ColorPicker/app/src/main/java/com/example/colorpicker/ui/settings/SettingsViewModel.java
@@ -0,0 +1,19 @@
+package com.example.colorpicker.ui.settings;
+
+
+import android.content.Intent;
+import android.view.View;
+
+import androidx.lifecycle.ViewModel;
+
+import com.example.colorpicker.HomePage;
+import com.example.colorpicker.MainActivity;
+
+public class SettingsViewModel extends ViewModel {
+
+    public SettingsViewModel() {
+
+
+    }
+
+}
